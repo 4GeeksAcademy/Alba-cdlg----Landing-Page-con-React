922a4321f307abfb19c66d6a5f3553d3476ff28f
Alba-cdlg <alba.cdelagrana@icloud.com>
2025-03-14 11:12:50 +0000

    con footer

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+import Home from "./js/components/Home"; // Importamos el componente Home
+
+function App() {
+  return (
+    <div>
+      <Home /> {/* Renderizamos el componente Home */}
+    </div>
+  );
+}
+
+export default App;
